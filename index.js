f1f0bb72c22a3c8404561d2dff26d60f941b21b0
Mykhailo <mishamoroz5@gmail.com>
2022-06-07 03:29:23 +0300

    blur & sharp filters

--- index.js
+++ index.js
@@ -70,6 +70,60 @@ const nullPixel = (imageData, pixel) => {
   return imageData;
 };
 
+
+const createImageData = (w,h) => {
+  const tmpCanvas = document.createElement('canvas');
+  const tmpCtx = tmpCanvas.getContext('2d');
+  return tmpCtx.createImageData(w,h);
+};
+
+
+
+const convolute = (pixels, weights) => {
+  const side = Math.round(Math.sqrt(weights.length));
+  const halfSide = Math.floor(side/2);
+  const src = pixels.data;
+  const sw = pixels.width;
+  const sh = pixels.height;
+  // pad output by the convolution matrix
+  const w = sw;
+  const h = sh;
+  const output = createImageData(w, h);
+  const dst = output.data;
+  const alphaFac = 1;
+  // go through the destination image pixels
+  for (let y=0; y<h; y++) {
+    for (let x=0; x<w; x++) {
+      let sy = y;
+      let sx = x;
+      let dstOff = (y*w+x)*4;
+      // calculate the weighed sum of the source image pixels that
+      // fall under the convolution matrix
+      let r=0, g=0, b=0, a=0;
+      for (let cy=0; cy<side; cy++) {
+        for (let cx=0; cx<side; cx++) {
+          let scy = sy + cy - halfSide;
+          let scx = sx + cx - halfSide;
+          if (scy >= 0 && scy < sh && scx >= 0 && scx < sw) {
+            let srcOff = (scy*sw+scx)*4;
+            let wt = weights[cy*side+cx];
+            r += src[srcOff] * wt;
+            g += src[srcOff+1] * wt;
+            b += src[srcOff+2] * wt;
+            a += src[srcOff+3] * wt;
+          }
+        }
+      }
+      dst[dstOff] = r;
+      dst[dstOff+1] = g;
+      dst[dstOff+2] = b;
+      dst[dstOff+3] = a + alphaFac*(255-a);
+    }
+  }
+  return output;
+};
+
+
 const task2a = () => {
   const taskDiv = document.createElement('div');
   taskDiv.className = 'row';
@@ -145,29 +199,33 @@ const task2d = () => {
 };
 
 
-const task2e = () => {
+const task2BlurGaussian = () => {
+  const size = 10;
+  const div = size * (size / 2);
+
+  const normalMatrix = new Array(size*size).fill(0).map(() => randn_bm() / div);
+  
   const taskDiv = document.createElement('div');
   taskDiv.className = 'row';
   const { ctx, cvs, imageData } = createNewCanvasFromImage();
-  const px = imageData.data;
-  const tmpPx = new Uint8ClampedArray(px.length);
-  tmpPx.set(px);
+  const imageDataNew = convolute(imageData, normalMatrix);
 
-  for (let i = 0; i < px.length; i++) {
-    if (i % 4 === 3) {continue;}
-    px[i] = ( tmpPx[i] 
-    + (tmpPx[i - 4] || tmpPx[i])
-    + (tmpPx[i + 4] || tmpPx[i]) 
-    + (tmpPx[i - 4 * imageData.width] || tmpPx[i])
-    + (tmpPx[i + 4 * imageData.width] || tmpPx[i]) 
-    + (tmpPx[i - 4 * imageData.width - 4] || tmpPx[i])
-    + (tmpPx[i + 4 * imageData.width + 4] || tmpPx[i])
-    + (tmpPx[i + 4 * imageData.width - 4] || tmpPx[i])
-    + (tmpPx[i - 4 * imageData.width + 4] || tmpPx[i])
-    ) / 9;
-  };
+  ctx.putImageData(imageDataNew, 0, 0);
+  taskDiv.appendChild(cvs);
+  document.body.appendChild(taskDiv);
+};
 
-  ctx.putImageData(imageData, 0, 0);
+const taskFilterSharp = () => {
+  const taskDiv = document.createElement('div');
+  taskDiv.className = 'row';
+  const { ctx, cvs, imageData } = createNewCanvasFromImage();
+  const imageDataNew = convolute(imageData,[ 
+    0, -1,  0,
+    -1,  5, -1,
+    0, -1,  0
+  ])
+
+  ctx.putImageData(imageDataNew, 0, 0);
   taskDiv.appendChild(cvs);
   document.body.appendChild(taskDiv);
 };
@@ -177,5 +235,6 @@ img.onload = () => {
   task2b();
   task2c();
   task2d();
-  task2e();
+  task2BlurGaussian();
+  taskFilterSharp();
 };
