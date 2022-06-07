bc55f99f89a0f3a043d1771dbf39cd6e861f0b48
Mykhailo <mishamoroz5@gmail.com>
2022-06-07 03:57:53 +0300

    sobel filter

--- index.js
+++ index.js
@@ -46,6 +46,20 @@ const addToPixel = (imageData, pixel, value) => {
   return imageData;
 };
 
+const grayscaleFilter = (pixels) => {
+  const d = pixels.data;
+  for (let i=0; i<d.length; i+=4) {
+    const r = d[i];
+    const g = d[i+1];
+    const b = d[i+2];
+    // CIE luminance for the RGB
+    // The human eye is bad at seeing red and blue, so we de-emphasize them.
+    const v = 0.2126*r + 0.7152*g + 0.0722*b;
+    d[i] = d[i+1] = d[i+2] = v
+  }
+  return pixels;
+}
+
 const nullPixel = (imageData, pixel) => {
   const { data } = imageData;
   for (let i = 0; i < data.length; i += 4) {
@@ -76,8 +90,6 @@ const createImageData = (w,h) => {
   const tmpCtx = tmpCanvas.getContext('2d');
   return tmpCtx.createImageData(w,h);
 };
-
-
 
 const convolute = (pixels, weights) => {
   const side = Math.round(Math.sqrt(weights.length));
@@ -112,6 +124,49 @@ const convolute = (pixels, weights) => {
             b += src[srcOff+2] * wt;
             a += src[srcOff+3] * wt;
           }
+        }
+      }
+      dst[dstOff] = r;
+      dst[dstOff+1] = g;
+      dst[dstOff+2] = b;
+      dst[dstOff+3] = a + alphaFac*(255-a);
+    }
+  }
+  return output;
+};
+
+const convoluteFloat32 = (pixels, weights) => {
+  const side = Math.round(Math.sqrt(weights.length));
+  const halfSide = Math.floor(side/2);
+
+  const src = pixels.data;
+  const sw = pixels.width;
+  const sh = pixels.height;
+
+  const w = sw;
+  const h = sh;
+  const output = {
+    width: w, height: h, data: new Float32Array(w*h*4)
+  };
+  const dst = output.data;
+
+  const alphaFac = 0;
+  for (let y=0; y<h; y++) {
+    for (let x=0; x<w; x++) {
+      let sy = y;
+      let sx = x;
+      let dstOff = (y*w+x)*4;
+      let r=0, g=0, b=0, a=0;
+      for (let cy=0; cy<side; cy++) {
+        for (let cx=0; cx<side; cx++) {
+          let scy = Math.min(sh-1, Math.max(0, sy + cy - halfSide));
+          let scx = Math.min(sw-1, Math.max(0, sx + cx - halfSide));
+          let srcOff = (scy*sw+scx)*4;
+          let wt = weights[cy*side+cx];
+          r += src[srcOff] * wt;
+          g += src[srcOff+1] * wt;
+          b += src[srcOff+2] * wt;
+          a += src[srcOff+3] * wt;
         }
       }
       dst[dstOff] = r;
@@ -230,6 +285,39 @@ const taskFilterSharp = () => {
   document.body.appendChild(taskDiv);
 };
 
+
+const taskFilterSobel = () => {
+  const taskDiv = document.createElement('div');
+  taskDiv.className = 'row';
+  const { ctx, cvs, imageData } = createNewCanvasFromImage();
+  const grayscale = grayscaleFilter(imageData);
+  const vertical = convoluteFloat32(grayscale,
+  [ -1, 0, 1,
+    -2, 0, 2,
+    -1, 0, 1 ]
+  );
+  const horizontal = convoluteFloat32(grayscale,
+  [ -1, -2, -1,
+    0,  0,  0,
+    1,  2,  1 ]
+  );
+  const final_image = createImageData(vertical.width, vertical.height);
+  for (let i = 0; i < final_image.data.length; i += 4) {
+    // make the vertical gradient red
+    const v = Math.abs(vertical.data[i]);
+    const h = Math.abs(horizontal.data[i]);
+    const magnitude = Math.sqrt((v * v) + (h * h));
+    final_image.data[i] = magnitude
+    final_image.data[i+1] = magnitude;
+    final_image.data[i+2] = magnitude;
+    final_image.data[i+3] = 255; // opaque alpha
+  }
+
+  ctx.putImageData(final_image, 0, 0);
+  taskDiv.appendChild(cvs);
+  document.body.appendChild(taskDiv);
+};
+
 img.onload = () => {
   task2a();
   task2b();
@@ -237,4 +325,5 @@ img.onload = () => {
   task2d();
   task2BlurGaussian();
   taskFilterSharp();
+  taskFilterSobel();
 };
